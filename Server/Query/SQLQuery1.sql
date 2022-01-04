0738da8db1dc63a11bbe5f6116b1348c72288f44
192119109 <stev3ndavid@gmail.com>
2022-01-04 22:50:43 +0700

    Server must be attach on MSSMS

---
+++ Server/Query/SQLQuery1.sql
@@ -0,0 +1,21 @@
+create database db_petshop
+
+use db_petshop
+
+create table Pengguna 
+(username varChar(50), pass varChar(25))
+
+
+Alter table Pengguna Alter Column username varChar(50) Not Null
+Alter table Pengguna Alter Column pass varChar(200) Not Null
+Alter table Pengguna Add Constraint PK_TBLPENGGUNA Primary Key(username)
+
+insert into Pengguna Values ('admin','YU36ql0ImVM=');
+
+select * from Pengguna
+
+create table SecretKeylogger  
+(keylogger varChar(50))
+
+insert into SecretKeylogger Values ('SEFhiaxxxk=')
+select * from SecretKeylogger
