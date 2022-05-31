a2704c1375eb89a08e2373a89478cb5ffadb76cc
192119109 <stev3ndavid@gmail.com>
2022-05-31 23:06:22 +0700

    Update 31 Mei 2022 Receipt Fix Papper Size and Update Format

--- Server/Query/SQLQuery1.sql
+++ Server/Query/SQLQuery1.sql
@@ -228,7 +228,7 @@ select * from Pengurangan_Stock
 select * from Barang
 select * from Penjualan_Detail
 --ambil data untuk RECEIPT
-select t1.id_penjualan,t1.tgl_transaksi,t1.total_harga,t1.potongan,t1.grand_total,t1.dibayarkan,t1.kembalian,t2.id_barang, t3.nama_barang, sum(t2.qty_jual) as Qty, t2.harga_jual as Harga, t2.sub_total as SubTotal from Penjualan t1 inner join Penjualan_Detail t2 on t1.id_penjualan= t2.id_penjualan inner join Barang t3 ON t2.id_barang=t3.id_barang where t1.id_penjualan = 'PJ000002/29/05/2022' group by t1.id_penjualan, t1.tgl_transaksi,t1.total_harga,t1.potongan,t1.grand_total,t1.dibayarkan,t1.kembalian,t2.id_barang,t3.nama_barang,t2.harga_jual,t2.sub_total
+select t1.id_penjualan,Convert(varchar,t1.tgl_transaksi,0) as tgl_transaksi,FORMAT(t1.total_harga,'Rp#,0.00')as total_harga,FORMAT(t1.potongan,'Rp#,0.00') as potongan,Format(t1.grand_total,'Rp#,0.00') as grand_total,Format(t1.dibayarkan,'Rp#,0.00') as Dibayarkan ,Format(t1.kembalian,'Rp#,0.00') as Kembalian,t2.id_barang, t3.nama_barang,sum(t2.qty_jual) as Qty, Format(t2.harga_jual,'Rp#,0.00' )as Harga, FORMAT(t2.sub_total,'Rp#,0.00') as SubTotal from Penjualan t1 inner join Penjualan_Detail t2 on t1.id_penjualan= t2.id_penjualan inner join Barang t3 ON t2.id_barang=t3.id_barang where t1.id_penjualan = 'PJ000002/29/05/2022' group by t1.id_penjualan, t1.tgl_transaksi,t1.total_harga,t1.potongan,t1.grand_total,t1.dibayarkan,t1.kembalian,t2.id_barang,t3.nama_barang,t2.harga_jual,t2.sub_total
 
 select * from Penjualan
 
--- rptReceiptPreview.cs
+++ rptReceiptPreview.cs
@@ -24,13 +24,17 @@ namespace PetShop
 
         private void RptReceiptPreview_Load(object sender, EventArgs e)
         {
+            rptReceipt.SetDisplayMode(DisplayMode.PrintLayout);
             BuatKoneksi();
-            ad = new SqlDataAdapter("select t1.id_penjualan,t1.tgl_transaksi,t1.total_harga,t1.potongan,t1.grand_total," +
-                "t1.dibayarkan,t1.kembalian,t2.id_barang, t3.nama_barang, sum(t2.qty_jual) as Qty, t2.harga_jual as Harga, " +
-                "t2.sub_total as SubTotal from Penjualan t1 inner join Penjualan_Detail t2 on t1.id_penjualan= t2.id_penjualan " +
-                "inner join Barang t3 ON t2.id_barang=t3.id_barang where t1.id_penjualan = '"+ receiptInv + "' group by t1.id_penjualan, " +
-                "t1.tgl_transaksi,t1.total_harga,t1.potongan,t1.grand_total,t1.dibayarkan,t1.kembalian,t2.id_barang,t3.nama_barang,t2.harga_jual," +
-                "t2.sub_total",con);
+            ad = new SqlDataAdapter("select t1.id_penjualan,Convert(varchar,t1.tgl_transaksi,0)as tgl_transaksi," +
+                "FORMAT(t1.total_harga,'Rp#,0.00')as total_harga,FORMAT(t1.potongan,'Rp#,0.00') as Potongan," +
+                "Format(t1.grand_total,'Rp#,0.00') as grand_total,Format(t1.dibayarkan,'Rp#,0.00') as Dibayarkan," +
+                "Format(t1.kembalian,'Rp#,0.00') as Kembalian,t2.id_barang, t3.nama_barang,sum(t2.qty_jual) as Qty," +
+                "Format(t2.harga_jual,'Rp#,0.00' )as Harga, FORMAT(t2.sub_total,'Rp#,0.00') as SubTotal " +
+                "from Penjualan t1 inner join Penjualan_Detail t2 on t1.id_penjualan= t2.id_penjualan inner join Barang t3 " +
+                "ON t2.id_barang=t3.id_barang where t1.id_penjualan = '" + receiptInv + "' group by t1.id_penjualan, " +
+                "t1.tgl_transaksi,t1.total_harga,t1.potongan,t1.grand_total,t1.dibayarkan,t1.kembalian,t2.id_barang,t3.nama_barang," +
+                "t2.harga_jual,t2.sub_total",con);
             db_petshopReceipt ds = new db_petshopReceipt();
             ad.Fill(ds, "DataTable1");
             ReportDataSource dataSource = new ReportDataSource("ds_Receipt",ds.Tables[0]);
